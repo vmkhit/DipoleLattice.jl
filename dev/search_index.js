260c2699fb44b699c9986d78334597b28512a423
Documenter.jl <documenter@juliadocs.github.io>
2022-04-11 17:58:15 +0000

    build based on d438e4a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DipoleLattice.jl","page":"DipoleLattice.jl","title":"DipoleLattice.jl","text":"","category":"section"},{"location":"","page":"DipoleLattice.jl","title":"DipoleLattice.jl","text":"Documentation for DipoleLattice.jl","category":"page"}]
+}
