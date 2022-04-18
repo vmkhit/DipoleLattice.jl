85173e27a9a38151bd94af69de7419511f0c41d1
vmkhit <vmkhitar@purdue.edu>
2022-04-18 16:58:31 -0400

    Fix some documentation

--- src/Lattice.jl
+++ src/Lattice.jl
@@ -3,43 +3,6 @@
   This data struct constructs 2D lattice object and outputs its properties.
   # Arguments
   - `id::String`: String that specify one of the five 2D bravis lattices or a gneral 2D lattice as described below.
-  # Description
-    The id argument takes the following values, id="S" for square, id="H" for hexagonal,
-    id="RC" for centered rectangular, id="RP" for primitive recangular and id="O" for
-    oblique lattices. If id is specified as an empty string id="" one has to specify the
-    direct lattice vectors ``a_{1}`` and ``a_{2}`` as an optional arguments. If id = "S"
-    or "H" one has to specify the lattice vector length as an optional argument. Similarly,
-    for the case when id="RC" or id="RP" one has to dpecify the lengths of the lattice vectors
-    along each orthogonal direction. For Oblique lattice, besides the lattice vector lengths
-    one has to specify also the angle between these vectors as the 3rd optional argumen. Below
-    we show examples of usage of all the lattice types.
-  # Example usage
-  * To define a square lattice with unit vector 5 we can call.
-  ```julia-repl
-    julia> L = Lattice("S", 5)
-  ```
-  * Hexagonal lattice with lattice vector length 4 can be defined as
-  ```julia-repl
-    julia> L = Lattice("S", 4)
-  ```
-  * Rectangular lattices with lattice vector lengths `a = 2`, `b = 4` can be defined as.
-  ```julia-repl
-    julia> L = Lattice("RC", a, b)
-  ```
-  ## or
-  ```julia-repl
-    julia> L = Lattice("RP", a, b)
-  ```
-  * The oblique lattice takes addtional argument ``\\psi`` the angle between the vectors,
-    assuming ``a_{1}`` is along the `x`-axis.
-  ```julia-repl
-    julia> L = Lattice("O", a, b, ψ)
-  ```
-  * Finally the general lattice with unit vectors ``a_{1} = [a_{1x}, a_{1y}]`` and
-  ``a_{1} = [a_{2x}, a_{2y}]``.
-  ```julia-repl
-    julia> L = Lattice("", a1, a2)
-  ```
 """
 struct Lattice2D
   id::String
